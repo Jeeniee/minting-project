44e50e0078ddd7f0117a38103a21fa8b2d8f1b0c
jayoung.lee <jayoung.lee@nway.com>
2022-07-19 17:04:43 +0900

    chore. update tsconfig

--- craco.config.js
+++ craco.config.js
@@ -1,27 +1,15 @@
 const emotionPresetOptions = {};
 
-const emotionBabelPreset = require('@emotion/babel-preset-css-prop').default(
-    undefined,
-    emotionPresetOptions,
+const emotionBabelPreset = require("@emotion/babel-preset-css-prop").default(
+  undefined,
+  emotionPresetOptions
 );
 
-const cracoAlias = require('craco-alias');
-
 module.exports = {
-    babel: {
-        plugins: [
-            ...emotionBabelPreset.plugins,
-            // your other plugins
-        ],
-    },
+  babel: {
     plugins: [
-		{
-			plugin: cracoAlias,
-			options: {
-				source: "tsconfig",
-				baseUrl: "./src",
-				tsConfigPath: "tsconfig.extend.json",
-			},
-		},
-	],
+      ...emotionBabelPreset.plugins,
+      // your other plugins
+    ],
+  },
 };
